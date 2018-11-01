fc96053481e8f93da623ca67b8b455d0da0b923f
Rikey Chen <rikey.chen16@gmail.com>
2018-10-31 22:57:40 -0700

    Add #fast_dance

--- W2D4/octopus.rb
+++ W2D4/octopus.rb
@@ -66,3 +66,18 @@ def slow_dance(direction, tiles)
   end
   nil
 end
+
+tiles_hash = {
+    "up" => 0,
+    "right-up" => 1,
+    "right"=> 2,
+    "right-down" => 3,
+    "down" => 4,
+    "left-down" => 5,
+    "left" => 6,
+    "left-up" => 7
+}
+
+def fast_dance(direction, tiles_hash)
+  tiles_hash[direction]
+end
