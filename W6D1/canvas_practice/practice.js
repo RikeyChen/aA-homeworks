b11dd148d02bfe9f0269e439e7518af244c0a88a
Rikey Chen <rikey.chen16@gmail.com>
2018-11-25 20:17:15 -0800

    Complete phase 3 of canvas exercise

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -6,4 +6,13 @@ document.addEventListener("DOMContentLoaded", function(){
   const ctx = canvasEl.getContext("2d");
   ctx.fillStyle = 'blue';
   ctx.fillRect(0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
+  ctx.strokeStyle('black');
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = 'green';
+  ctx.fill();
+
 });
--- W6D1/canvas_practice/solution.js
+++ W6D1/canvas_practice/solution.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
   ctx.beginPath();
   ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
-  ctx.strokeStyle = "green";
+  ctx.strokeStyle("green");
   ctx.lineWidth = 5;
   ctx.stroke();
   ctx.fillStyle = "blue";
