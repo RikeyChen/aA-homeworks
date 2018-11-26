a525db4a1933d3dc8850419fdf098c45533e24a1
Rikey Chen <rikey.chen16@gmail.com>
2018-11-25 20:10:07 -0800

    Complete phase 2 canvas exercise

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
   const canvasEl = document.getElementById('mycanvas');
   canvasEl.width = 500;
   canvasEl.height = 500;
-  const ctx = canvasEl.getContext("2d");
 
+  const ctx = canvasEl.getContext("2d");
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0, 0, 500, 500);
 });
