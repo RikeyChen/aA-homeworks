83e337bcddf8d18c2f51e3ed1f4a7aebba2f2abb
Rikey Chen <rikey.chen16@gmail.com>
2018-11-29 22:23:13 -0800

    Add buttons for math operators to #render

--- W6D5/react_hw/calculator.jsx
+++ W6D5/react_hw/calculator.jsx
@@ -6,6 +6,10 @@ class Calculator extends React.Component {
     this.state = { result: 0, num1: "", num2: "" };
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this.addNums = this.addNums.bind(this);
+    this.subtractNums = this.subtractNums.bind(this);
+    this.multiplyNums = this.multiplyNums.bind(this);
+    this.divideNums = this.divideNums.bind(this);
   }
 
   // your code here
@@ -13,7 +17,14 @@ class Calculator extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.state.result}</h1> // replace this with your code
+        <h1>{this.state.result}</h1>
+        <input onChange={this.setNum1} value={this.state.num1}/>
+        <input onChange={this.setNum2} value={this.state.num2}/>
+        <br />
+        <button onClick={this.addNums}>+</button>
+        <button onClick={this.subtractNums}>-</button>
+        <button onClick={this.mutiplyNums}>*</button>
+        <button onClick={this.divideNums}>/</button>
       </div>
     );
   }
@@ -26,6 +37,22 @@ class Calculator extends React.Component {
   setNum2(e) {
     const num2 = e.target.value ? parseInt(e.target.value) : ""
     this.setState({ num2 });
+  }
+
+  addNums(e) {
+
+  }
+
+  subtractNums(e) {
+
+  }
+
+  multiplyNums(e) {
+
+  }
+
+  divideNums(e) {
+
   }
 
 }
