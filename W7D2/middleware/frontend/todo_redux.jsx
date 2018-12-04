7c3e28e6295a2f5273893ae5ef5fb152d08ca5a8
Rikey Chen <rikey.chen16@gmail.com>
2018-12-03 21:41:20 -0800

    Refactor to use Redux#applyMiddleware in store.js for phase III

--- W7D2/middleware/frontend/todo_redux.jsx
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -4,27 +4,11 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = store => next => (action) => {
-  console.log(store.getState());
-  console.log(action);
-  next(action);
-  console.log(store.getState());
-};
-
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-  middlewares.forEach((mw) => {
-    dispatch = mw(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch });
-};
-
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state
     ? JSON.parse(localStorage.state)
     : {};
-  let store = configureStore(preloadedState);
-  store = applyMiddlewares(store, addLoggingToDispatch);
+  const store = configureStore(preloadedState);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
