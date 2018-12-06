6f805c5a91605055b049e57b07a8b97182540c0a
Rikey Chen <rikey.chen16@gmail.com>
2018-12-05 22:41:14 -0800

    Complete phase II

--- W7D4/rainbow_routes/components/blue.jsx
+++ W7D4/rainbow_routes/components/blue.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { Route, Link, NavLink } from "react-router-dom";
-import Indigo from "./indigo";
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Indigo from './indigo';
 
 class Blue extends React.Component {
   render() {
     return (
       <div>
         <h2 className="blue" />
-        {/* Links here */}
+        {<Link to="/blue">Blue only</Link>}
+        {<Link to="/blue/indigo">Add indigo</Link>}
 
         {<Route path="/blue/indigo" component={Indigo} />}
       </div>
--- W7D4/rainbow_routes/components/red.jsx
+++ W7D4/rainbow_routes/components/red.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Route, Link, NavLink } from 'react-router-dom';
-import Orange from './orange';
-import Yellow from './yellow';
+import React from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+import Orange from "./orange";
+import Yellow from "./yellow";
 
 class Red extends React.Component {
   render() {
     return (
       <div>
         <h2 className="red" />
-        {/* Links here */}
+        {<Link to="/red">Red only</Link>}
+        {<Link to="/red/orange">Add orange</Link>}
+        {<Link to="/red/yellow">Add yellow</Link>}
 
         {<Route path="/red/orange" component={Orange} />}
         {<Route path="/red/yellow" component={Yellow} />}
